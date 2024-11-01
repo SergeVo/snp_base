656f49e9e6bb695231cfcdf826e8007df8fa8531
Сергей Догадин <me@sdogadin.ru>
2024-11-01 16:58:21 +0300

    task_01 added

--- task_01.py
+++ task_01.py
@@ -20,9 +20,8 @@ def is_palindrome(string):
     return chars == chars[::-1]
 
 
-# print(is_palindrome("A man, a plan, a canal -- Panama"))
-# print(is_palindrome("Madam, I'm Adam!"))
-# print(is_palindrome(333))
-# print(is_palindrome(None))
-# print(is_palindrome("Abracadabra"))
-print(is_palindrome("лазер боре хер обрезал"))
+print(is_palindrome("A man, a plan, a canal -- Panama"))
+print(is_palindrome("Madam, I'm Adam!"))
+print(is_palindrome(333))
+print(is_palindrome(None))
+print(is_palindrome("Abracadabra"))
