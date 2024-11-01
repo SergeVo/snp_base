a3e0be4489a777b27c3c5ffe08c54fad4e31e5f3
Сергей Догадин <me@sdogadin.ru>
2024-11-01 16:13:58 +0300

    task_01 added

---
+++ task_01.py
@@ -0,0 +1,28 @@
+# Разработайте метод is_palindrome(string), который будет определять, является ли
+# параметр string палиндромом (строкой, которая читается одинаково как сначала
+# так и с конца), при условии игнорирования пробелов, знаков препинания и
+# регистра.
+
+def is_palindrome(string):
+    if string is None:
+        return False
+
+    if isinstance(string, (int, float)):
+        string = str(string)
+
+    chars = []
+
+    for char in string.lower():
+        if char.isalnum():
+            chars.append(char)
+
+    print(string, chars)
+    return chars == chars[::-1]
+
+
+# print(is_palindrome("A man, a plan, a canal -- Panama"))
+# print(is_palindrome("Madam, I'm Adam!"))
+# print(is_palindrome(333))
+# print(is_palindrome(None))
+# print(is_palindrome("Abracadabra"))
+print(is_palindrome("лазер боре хер обрезал"))
