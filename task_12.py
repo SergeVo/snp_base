ba36924d880bb2a2450214fd3368d952b6a1c84a
Сергей Догадин <me@sdogadin.ru>
2024-11-08 16:31:32 +0300

    task_12 updated

--- task_12.py
+++ task_12.py
@@ -4,35 +4,36 @@
 # калорийности десерта менее 200) и is_delicious (возвращает true для всех
 # десертов).
 
-class Dessert:
+# class Dessert:
 
-    def __init__(self, name=None, calories=None):
-        self.name = name
-        self.calories = calories
+#     def __init__(self, name=None, calories=None):
+#         self.name = name
+#         self.calories = calories
 
-    def get_name(self):
-        return self.name
+#     def get_name(self):
+#         return self.name
 
-    def get_calories(self):
-        return self.calories
+#     def get_calories(self):
+#         return self.calories
 
-    def set_name(self, name):
-        self.name = name
+#     def set_name(self, name):
+#         self.name = name
 
-    def set_calories(self, calories):
-        self.calories = calories
+#     def set_calories(self, calories):
+#         self.calories = calories
 
-    def is_healthy(self):
-        return self.calories < 200
+#     def is_healthy(self):
+#         return self.calories < 200
 
-    def is_delicious(self):
-        return True
+#     def is_delicious(self):
+#         return True
 
-    def __str__(self):
-        return (f'Название: {self.name}; '
-                f'Калории: {self.calories}; '
-                f'Полезность: {self.is_healthy()}; '
-                f'Вкусненько? {self.is_delicious()}.')
+#     def __str__(self):
+#         return (f'Название: {self.name}; '
+#                 f'Калории: {self.calories}; '
+#                 f'Полезность: {self.is_healthy()}; '
+#                 f'Вкусненько? {self.is_delicious()}.')
+from task_11 import Dessert
 
 
 class Jellybean(Dessert):
